1344306fed7f36523c91b5799f2d96709f97c1a1
VirginieData <124630160+VirginieData@users.noreply.github.com>
2023-03-30 19:31:47 +0200

    Add files via upload

---
+++ 1_🍷_Contexte.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+# création logo en haut sidebar
+add_logo("https://res.cloudinary.com/wildcodeschool/image/upload/c_fill,h_50/v1/static/irjoy97aq0eol8bf6959")
+
+
+# Contexte Domaine des Croix
+# adresse
+# photo
+# quelques infos rapides
+st.image("https://wineguildeducation.org/wp-content/uploads/2020/03/domaine-de-croix.jpg")
+st.image("https://zupimages.net/up/23/13/q7a4.png")
+st.write("")
+st.write("David Croix dispose de belles parcelles sur les appellations Beaune, Beaune 1er Cru, Aloxe-Corton, Savigny-les-Beaune et les grands crus Corton et Corton-Charlemagne, qu’il travaille avec un bon sens paysan profond, dans une logique proche de celle de l’agriculture biologique, quoique sans certification. Aucun produit de synthèse ne trouve place dans ses vignes. David Croix cherche avant tout à produire de vins souples, élégants tout en n’étant pas dénués de matière. Il n’hésite pas à égrapper assez fortement ses raisins et à piger beaucoup ses rouges afin de « remplir le vin » et de lui apporter de l’onctuosité. Ce sont donc à des pinots charmeurs en diable que nous avons affaire, des vins accessibles dès leur jeunesse, élégants et très faciles à boire malgré leur belle complexité. Un petit domaine qui n’a rien à envier aux grands noms !")
+st.caption('propos de "La cave de Bacchus"')
+# Ventes aux US
+
+st.title('Le marché des vins aux Etats-Unis')
+st.write("Le marché du vin aux États-Unis est le plus grand du genre au monde en raison de la forte population, soutenu par un revenu disponible élevé. De plus, les vins sont populaires dans le pays depuis des siècles et le marché local a progressé avec la migration de personnes issues de milieux culturels vivants, introduisant de nouvelles saveurs sur le marché. La premiumisation des produits viticoles couplée à l'innovation en matière de saveur a encore régulé le marché du vin dans la région.")
+st.write('# Quelques chiffres à observer :')
+st.write("* 80% de la consommation centrée sur 5 états (Californie, New York + New Jersey, Floride, Texas et Illinois)")
+st.write("* Le système des trois tiers hérité de la prohibition (Fournisseur - Grossiste - Détaillant + Restaurateur - Consommateur)")
+st.write("* 3ème marché mondial : environ 18 x la superficie de la France")
+st.write("* 36% de la population américaine consommateurs de vin : 120M")
+
+st.title('Se préparer pour vendre aux Etats-Unis')
+st.write(" Vendre aux Etats-Unis ne s'improvise pas. Il sera nécessaire de bien définir la feuille de route")
+st.write("* Comprendre et définir les objectifs")
+st.write ("* Touver la stratégie et le représentant adaptés")
+st.write("* Comprendre les règles de ventes aux Etats-Unis")
+st.write("* Evaluer son positionnement tarifaire")
+st.write("")
+st.caption("infos pratiques : https://www.vinsvaldeloire.fr/sites/default/files/pdf/presentation-marche-usa-sival2020.pdf")
+
